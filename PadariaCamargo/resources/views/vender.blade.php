12867a7f8faa77f0dff5a5a7331da1b42816a578
Vinicius <telles861@gmail.com>
2023-08-23 15:45:34 -0300

    Projeto parte 1

---
+++ PadariaCamargo/resources/views/vender.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" type="text/css" href="css/style.css"/>
+        <title>PadariaCamargo</title>
+    </head>
+    <body>
+        <div id="cabecalho">
+            <a href="/">pagina1</a>
+        </div>
+        <div id="conteudo">
+            <form id="formVenda" method="post" action="pagina3.html">
+                <div class="exibeProdutos">produtos</div>
+                <input type="reset" value="Limpar ✖">
+                <input type="submit" value="Avançar ✔">
+            </form>
+        </div>
+        <div id="rodape"></div>
+    </body>
+</html>
---
+++ PadariaCamargo/routes/web.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+Route::get('vender', function () {
+    return view('vender');
+});
+Route::get('regProduto', function () {
+    return view('regProduto');
+});
+Route::get('historico', function () {
+    return view('historico');
+});
